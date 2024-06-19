a8a40f9ab6ddd1fe61614cf0a3a85e28aedc4844
baaaaaaaaasowenyaaaaaaamamabeatsebaaah <24956911+baaaaaaaaasowenyaaaaaaamamabeatsebaaah@users.noreply.github.com>
2024-06-19 15:38:58 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: './.storybook/main.js',
-    preview: './.storybook/preview.js',
-  },
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -27,19 +23,6 @@ module.exports = {
         use: 'file-loader',
       },
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      chunks: ['main'],
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    port: 8080,
   },
   resolve: {
     extensions: ['.js'],
