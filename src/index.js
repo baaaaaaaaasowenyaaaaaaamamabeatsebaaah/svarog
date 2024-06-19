3c915e263a93515814478d677dbdd724ddc28c89
baaaaaaaaasowenyaaaaaaamamabeatsebaaah <24956911+baaaaaaaaasowenyaaaaaaamamabeatsebaaah@users.noreply.github.com>
2024-06-19 15:02:43 +0200

    fix github workflow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 export { default as Button } from './components/Button/Button';
-export { default as Sidebar } from '../.storybook/components/Sidebar/Sidebar';
-export { default as Stage } from '../.storybook/components/Stage/Stage';
