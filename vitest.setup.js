7ba01bced5bb0c92c7ec52d8ec759e3ee5369f88
Sebastian Huber <24956911+baaaaaaaaasowenyaaaaaaamamabeatsebaaah@users.noreply.github.com>
2025-03-06 14:21:09 +0100

    Fix eslint build errors

---
+++ vitest.setup.js
@@ -0,0 +1,7 @@
+// vitest.setup.js
+// Global setup for vitest tests
+
+// Add any global setup code here
+// For example, mocking browser globals that might not be in jsdom
+
+// This file is referenced in vitest.config.js
