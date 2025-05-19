d1a02476a816da11af5831dc9593ac01d2ad2eaa
baaaaaaaaasowenyaaaaaaamamabeatsebaaah <baaaaaaaaasowenyaaaaaaamamabeatsebaaah@users.noreply.github.com>
2025-05-19 19:58:01 +0200

    refactor: add missing index.js

---
+++ src/components/Card/index.js
@@ -0,0 +1,5 @@
+// src/components/Card/index.js
+import Card from './Card.js';
+
+export default Card;
+export { Card };
---
+++ src/components/Link/index.js
@@ -0,0 +1,5 @@
+// src/components/Link/index.js
+import Link from './Link.js';
+
+export default Link;
+export { Link };
